8193a511012c94dd571ab0768c839418d2649997
Yassine7009 <169166942+TheOnlyYassine@users.noreply.github.com>
2024-05-14 16:19:55 +0200

    op

--- app.py
+++ app.py
@@ -1,81 +1,53 @@
-from flask import Flask, jsonify, request, render_template
-from werkzeug import secure_filename
-import os
+from flask import Flask, send_file
 
-from utils import allowed_file, convert_html_to_pdf, convert_doc_to_pdf
+import asyncio
+
+from pyppeteer import launch
+
+ 
 
 app = Flask(__name__)
 
+ 
 
-@app.route('/', methods=['GET'])
-def index():
-    """ Renders Index.html """
-    #cinve
-    try:
-        return render_template('index.html')
-    except Exception as e:
-        print("Exception Occurred", e)
-        return jsonify({"status": "failed", "message": "Something Went Wrong !!"})
+async def generate_pdf(url, output_file):
 
+    browser = await launch()
 
-from flask import send_file
+    page = await browser.newPage()
 
-@app.route('/upload', methods=['POST'])
-def file_converter():
-    """
-    Function Processing Steps:
-    Step-1 : Check uploaded file extension ,if accepted format process further
-    Step-2 : Save the files into uploads folder
-    Step-3 : Convert the html,doc and docx files into pdf file and stores into converted_files folder
+    await page.goto(url)
 
-    Note :  If file is already in pdf format than file will directly save in converted_files
-            folder without other action.
-    """
-    if request.method == "POST":
-        try:
-            files = request.files.getlist('file')
-            print("files", files)
-            if len(files) > 0:
-                for data in files:
-                    if allowed_file(data.filename):
-                        filename = secure_filename(data.filename)
-                        extension = filename.split('.')
-                        file_path = os.path.join('static/uploads', filename)
+    await page.pdf({'path': output_file, 'format': 'A4'})
 
-                        if extension[-1] == 'pdf':
-                            pdf_file_path = os.path.join('static/converted_files', filename)
-                            data.save(pdf_file_path)
-                        else:
-                            data.save(file_path)
+    await browser.close()
 
-                        if extension[-1] == 'html':
-                            if convert_html_to_pdf(file_path, extension[0]):
-                                print("File Converted to PDF Successfully !!")
-                            else:
-                                raise Exception('Something Went Wrong !')
+ 
 
-                        elif extension[-1] == "docx" or extension[-1] == "doc":
-                            if convert_doc_to_pdf(file_path):
-                                print("File Converted to PDF Successfully !!")
-                            else:
-                                raise Exception('Something Went Wrong !')
-                        
-                        # Return the converted file to the user
-                        return send_file(pdf_file_path, as_attachment=True, attachment_filename=f"{extension[0]}.pdf")
+@app.route('/generate_pdf')
 
-                    else:
-                        return jsonify({"status": "failed", "message": "Format Not Allowed !!"})
-            else:
-                return jsonify({"status": "failed"})
-        except Exception as e:
-            return jsonify({"status": "failed", "message": str(e)})
+def generate_pdf_endpoint():
 
-        except Exception as e:
-            print("Exception Occurred", e)
-            return jsonify({"status": "exception", "message": "Something Went Wrong !!"})
-    else:
-        return jsonify({"status": "failed", "message": "Method Not Allowed !"})
+    # URL to convert to PDF
 
+    url = 'https://diot-siaci-analytics.com/rapportFictif/alerting_sismique/'
+
+    # Output file path
+
+    output_file = 'kk.pdf'
+
+   
+
+    asyncio.run(generate_pdf(url, output_file))
+
+   
+
+    # Return the PDF file as a response
+
+    return send_file(output_file, as_attachment=True)
+
+ 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000)
+
+    app.run(debug=True)
